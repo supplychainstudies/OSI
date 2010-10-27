af87d3cefe9ce34fe2a17fb6e7b42bc2dd04eb2e
Jorge Zapico <zapico@kth.se>
2010-10-27 16:37:06 +0200

    Added gitignore

--- application/views/info_view.php
+++ application/views/info_view.php
@@ -14,14 +14,9 @@
 	<?= $navigationDisplay;?>
 
 	<div id="columnleft">
-<p>OpenSustainability.info will facilitate the conversion of environmental data to a linked data format for the purpose of making data more accessible for everyone and easier to share, compare, and analyze. We will create:</p>
-<h2>1.An Open Database of Sustainability Information</h2>
+<h2>An Open Database of Sustainability Information</h2>
 <p>Our database will contain environmental and social impact information for resources, processes and products. This information will be stored as linked data so that it is compatible with existing (and emerging) standards for sustainability accounting. Our database will be accessible remotely through an API, published as Open Data, and accessible through an App Garden of specialized sustainability software. </p>
-<h2>2.Community</h2>
 <p>Creating and nurturing a community of environmental experts that create, discuss and evaluate the data. Giving them the technological affordances and the motivation to open up their data and share.</p>
-<h2>3.Standards</h2>
-<p>Providing and promoting open standards for sustainability information of resources, processes and products. Creating metadata structures and developing them in a collaborative way with the community.</p>
-	
 	</div>
 	<div id="columnright">
 		
