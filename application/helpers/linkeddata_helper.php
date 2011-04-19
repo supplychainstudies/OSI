1818318e708905331ae40603dbac199cabaec3fb
bianca <bianca.sayan@gmail.com>
2011-04-19 18:36:55 -0400

    The linked data helper has a third parameter that allows you to pick the type of label (if it is available)

--- application/helpers/linkeddata_helper.php
+++ application/helpers/linkeddata_helper.php
@@ -1,10 +1,13 @@
 <?php if(!defined('BASEPATH')) { exit('No direct script access allowed'); }
 
-	function linkThis($uri, $tooltips) {
+	function linkThis($uri, $tooltips, $label = "l") {
 		if (isset($tooltips[$uri]) == true) {
-			return "<a class=\"tooltip\" id=\"" . $uri . "\">" . $tooltips[$uri]['l'] . "</a>";
+			if (isset($tooltips[$uri][$label]) != true) {
+				$label = "l";
+			}
+			return "<a class=\"tooltip\" id=\"" . $uri . "\">" . $tooltips[$uri][$label] . "</a>";
 		} elseif (strpos($uri, ":") !== false) {
-			$xarray = explode($uri, ":");
+			$xarray = explode(":", $uri);
 			return $xarray[1];
 		} else {
 			return $uri;
