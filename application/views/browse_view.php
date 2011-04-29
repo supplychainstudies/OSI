a3a4b21d5143aeedb834ce7994d9161a9e3d8d70
Jorge Zapico <zapico@kth.se>
2011-04-29 21:21:38 +0000

    Working with masonry

--- application/views/browse_view.php
+++ application/views/browse_view.php
@@ -1,4 +1,5 @@
 <? $this->load->helper("linkeddata_helper"); ?>
+<? $this->load->helper("impact_helper"); ?>
 <!DOCTYPE html>
 <?=$metaDisplay;?>
 <html>
@@ -7,65 +8,63 @@
 	
 	<?=$styles;?>
 	<?=$headerDisplay;?>
-</head>
 
+	<script type="text/javascript" src="/assets/scripts/jquery/jquery-1.5.1.min.js"></script>
+	<script type="text/javascript" src="/assets/scripts/jquery/jquery-ui-1.8.11.custom.min.js"></script>
+	<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+	<script type="text/javascript" src="http://footprinted.org/assets/scripts/jquery/jquery.masonry.min.js"></script>
+	<script type="text/javascript" src="/assets/scripts/jquery/jquery.masonry.js"></script>
+	
+		<script>		
+		
+		$(function() {
+			$( ".square" ).click(function(){
+				if (typeof opensquare!="undefined"){
+					opensquare.removeClass("big");
+					opensquare.removeClass("grey");
+	                opensquare.addClass("small");
+					opensquare.addClass("blue");
+					opensquare.load('/lca/getName/'+opensquare.attr('id'));
+				}
+				opensquare = $(this);
+                $(this).removeClass("small");
+				$(this).removeClass("blue");
+                $(this).addClass("big");
+				$(this).addClass("grey");
+				$(this).parent().masonry({	  
+					  singleMode: true, 
+					  itemSelector: '.square'});
+				}
+				$(this).load('/lca/getImpacts/'+$(this).attr('id'));
+
+				$('#columnleft').masonry();
+				return false;	
+		
+		
+			});
+			
+		});
+		
+
+		</script>
+
+</head>
 <body id="home">
 	<div id ="contentwrap">	
 	<?= $navigationDisplay;?>
 	
 	<div id="columnleft">
-		<div class="big grey square"><p>
-			<h2>Footprint of one kilogram of Aluminum</h2>
-			<? foreach ($feature_info['impactAssessments'] as $impactAssessment) {
-				
-				
-				switch ($impactAssessment['impactCategoryIndicator']) {
-				    case 'ossia:waste':
-				        $color = "#C5E9FF";
-						$max = 10;
-						break;
-				    case 'ossia:CO2e':
-				        $color = "#5AC0FF";
-						$max = 10;
-						break;
-				    case "ossia:energy":
-				        $color = "#227CAF";
-						$max = 500;
-						break;
-					case "ossia:water":
-					    $color = "#45A3D8";
-						$max = 10;
-						break;
-					default:
-						$color = "#45A3D8";
-						$max = 50;
-				}
-				$size = 2* round(50*$impactAssessment['amount']/$max);
-				if ($size > 80) { $size = 80; }
-				$margin = (100-$size)/2;
-				$margintop = (100-$size)/6;
-
-				echo '<div class="impact"><div class="circle"><div style="width:'.$size.'px; height:'.$size.'px;margin-left:'.$margin.'px;margin-top:'.$margintop.'px; background:'.$color.'; -moz-border-radius: 40px; -webkit-border-radius:40px;"></div></div>';
-				echo '<div class="nr"><h1 class="nr">' . round($impactAssessment['amount'],2) . '</h1></div>';
-				echo '<div class="meta"><p class="unit">'. $impactAssessment['unit'] .'</p><p class="category">';
-				echo  $impactAssessment['impactCategoryIndicator'];
-				echo "<p/></div></div>"; 
-				
-			}?>
-		</p></div>
 		<div class="medium aoi square">
 			<h1 class="bignr">456</h1>
 			<p>Footprints available</p>
 		</div>
-		<div id="all_resources">
 				<?
 					foreach ($set as $row) {
 						// Remove the opensustainability part of the url
 						$myString = str_replace ("http://footprinted.org/rdfspace/lca/", "", $row['link']);
-						echo '<div class="small blue square"><p><a href="/lca/view/'.$myString.'">'.$row['name'].'</a><p/></div>';
+						echo '<div class="small blue square" id="'.$myString.'"><p><a href="/lca/view/'.$myString.'">'.$row['name'].'</a><p/></div>';
 					}
 				?>
-		</div>
 	</div>
 
 
