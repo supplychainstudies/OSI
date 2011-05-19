9ccde64119600d4de5f88e50a6aa147814530544
bianca <bianca.sayan@gmail.com>
2011-05-19 15:20:47 -0400

    fix loadmodel

--- application/models/loadmodel.php
+++ application/models/loadmodel.php
@@ -6,7 +6,6 @@ class Loadmodel extends FT_Model{
 	
 	
 	public function dumpqudt() {
-		var_dump($this->arc_config);
 		$this->arc_config['store_name'] = 'qudt';
 		$cached = array(
 			array(
@@ -57,87 +56,87 @@ class Loadmodel extends FT_Model{
 		$this->arc_config['store_name'] = 'eco';
 		$cached = array(
 			array(
-				'http://osi/assets/schemas/Earthster/alloc.n3',
+				'http://footprinted.org/assets/schemas/Earthster/alloc.n3',
 				'http://ontology.earthster.org/eco/alloc#'
 			),				
 			array(
-				'http://osi/assets/schemas/Earthster/attribute.n3',
+				'http://footprinted.org/assets/schemas/Earthster/attribute.n3',
 				'http://ontology.earthster.org/eco/attribute#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/biboBridge.n3',
+				'http://footprinted.org/assets/schemas/Earthster/biboBridge.n3',
 				'http://ontology.earthster.org/eco/biboBridge#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/bridges.n3',
+				'http://footprinted.org/assets/schemas/Earthster/bridges.n3',
 				'http://ontology.earthster.org/eco/bridges#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/cml2001.ttl',
+				'http://footprinted.org/assets/schemas/Earthster/cml2001.ttl',
 				'http://ontology.earthster.org/eco/cml2001#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/core.n3',
+				'http://footprinted.org/assets/schemas/Earthster/core.n3',
 				'http://ontology.earthster.org/eco/core#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/ecodl.n3',
+				'http://footprinted.org/assets/schemas/Earthster/ecodl.n3',
 				'http://ontology.earthster.org/eco/ecodl#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/ecofull.n3',
+				'http://footprinted.org/assets/schemas/Earthster/ecofull.n3',
 				'http://ontology.earthster.org/eco/ecofull#'
 			),				
 			array(
-				'http://osi/assets/schemas/Earthster/ecoinvent.ttl',
+				'http://footprinted.org/assets/schemas/Earthster/ecoinvent.ttl',
 				'http://ontology.earthster.org/eco/ecoinvent#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/ecospold.n3',
+				'http://footprinted.org/assets/schemas/Earthster/ecospold.n3',
 				'http://ontology.earthster.org/eco/ecospold#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/fasc.n3',
+				'http://footprinted.org/assets/schemas/Earthster/fasc.n3',
 				'http://ontology.earthster.org/eco/fasc#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/foafBridge.n3',
+				'http://footprinted.org/assets/schemas/Earthster/foafBridge.n3',
 				'http://ontology.earthster.org/eco/foafBridge#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/fullAxioms.n3',
+				'http://footprinted.org/assets/schemas/Earthster/fullAxioms.n3',
 				'http://ontology.earthster.org/eco/fullAxioms#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/goodRelationsBridge.n3',
+				'http://footprinted.org/assets/schemas/Earthster/goodRelationsBridge.n3',
 				'http://ontology.earthster.org/eco/goodRelationsBridge#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/ilcd.ttl',
+				'http://footprinted.org/assets/schemas/Earthster/ilcd.ttl',
 				'http://ontology.earthster.org/eco/ilcd#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/impact.n3',
+				'http://footprinted.org/assets/schemas/Earthster/impact.n3',
 				'http://ontology.earthster.org/eco/impact#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/impact2002Plus.n3',
+				'http://footprinted.org/assets/schemas/Earthster/impact2002Plus.n3',
 				'http://ontology.earthster.org/eco/impact2002Plus#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/sumoBridge.n3',
+				'http://footprinted.org/assets/schemas/Earthster/sumoBridge.n3',
 				'http://ontology.earthster.org/eco/sumoBridge#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/timeBridge.n3',
+				'http://footprinted.org/assets/schemas/Earthster/timeBridge.n3',
 				'http://ontology.earthster.org/eco/timeBridge#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/uncertaintyDistribution.n3',
+				'http://footprinted.org/assets/schemas/Earthster/uncertaintyDistribution.n3',
 				'http://ontology.earthster.org/eco/uncertaintyDistribution#'
 			),
 			array(
-				'http://osi/assets/schemas/Earthster/unit.ttl',
+				'http://footprinted.org/assets/schemas/Earthster/unit.ttl',
 				'http://ontology.earthster.org/eco/unit#'
 			),
 		);
