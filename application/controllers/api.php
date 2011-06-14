bddf23766b648f96f1efeed4ca901a8bf1bb78b5
Jorge Zapico <zapico@kth.se>
2011-06-14 20:22:42 +0000

    Using chached database

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -13,212 +13,65 @@
 class API extends FT_Controller {
 	public function API() {
 		parent::__construct();
-		$this->load->model(Array('lcamodel', 'geographymodel', 'bibliographymodel','peoplemodel','commentsmodel','ecomodel','opencycmodel'));		
-		$this->load->library(Array('form_extended','SimpleLoginSecure'));
-		$this->load->helper(Array('linkeddata_helper'));
+		//$this->load->model(Array('lcamodel', 'geographymodel', 'bibliographymodel','peoplemodel','commentsmodel','ecomodel','opencycmodel'));		
+		//$this->load->library(Array('form_extended','SimpleLoginSecure'));
+		//$this->load->helper(Array('linkeddata_helper'));
 	}
 	public $URI;
 	public $data;
 	public $post_data;
-	public $tooltips = array();
 
-
+	//General search 
 	public function search($encode = "json") {
 		parse_str($_SERVER['QUERY_STRING'],$_GET);
 		$checked_URIs = array();
 		$search_terms = $_GET;
 		$limit = 20;
 		$offset = 0;
-		$value = null;	
+		$value = null;
 		$URIs = array();
 		$rs = array();
+		$category = null;
+		$name = null;
 		if (isset($search_terms['limit']) == true) {
 			$limit = $search_terms['limit'];
 		}
-		
 		if (isset($search_terms['offset']) == true) {
 			$offset = $search_terms['offset'];
 		}
-		
-		if (isset($search_terms['product']) == true) {
-			$value = $search_terms['product'];
+		if (isset($search_terms['name']) == true) {
+			$name = $search_terms['name'];
 		}	
-		if (isset($search_terms['encode']) == true) {
-			$encode = $search_terms['encode'];
-		}	
-		$results = $this->lcamodel->simpleSearch($value, $limit, $offset);
-		foreach ($results as $result) {
-			$impacts = array (
-				'uri' => $result['uri'],
-				'impactAssessments' => $this->lcamodel->convertImpactAssessments($this->lcamodel->getImpactAssessments($result['uri'])),
-				'quantitativeReference' => $this->lcamodel->convertQR($this->lcamodel->getQR($result['uri']))
-				);
-			//Normalize the results to 1 kg
-			$rs[$result['uri']] = $this->normalize($impacts);
-			
-		}
-
-		if ($encode == 'json') {
-			header('Content-type: application/json');
-			echo json_encode($rs);
-		} else if ($encode == 'rdf') {
-			header('Content-type: text/xml');
-			echo $rs;			
-		} else if ($encode == 'xml') {
-			header('Content-type: text/xml');
-			echo $this->assocArrayToXML("results",$rs);
-		} else if ($encode == 'html') {
-			header('Content-type: text/html');
-			var_dump($rs);			
-		}
-		
-	}
-	
-	public function searchByCategory($encode = "json") {
-		parse_str($_SERVER['QUERY_STRING'],$_GET);
-		$checked_URIs = array();
-		$search_terms = $_GET;
-		$limit = 20;
-		$offset = 0;
-		$value = null;	
-		$URIs = array();
-		$results = array();
-		$rs = array();
-		if (isset($search_terms['limit']) == true) {
-			$limit = $search_terms['limit'];
-		}
-		
-		if (isset($search_terms['offset']) == true) {
-			$offset = $search_terms['offset'];
-		}
 		if (isset($search_terms['encode']) == true) {
 			$encode = $search_terms['encode'];
 		}
 		if (isset($search_terms['category']) == true) {
-			$value = array($search_terms['category']);
-			$opencyc = $this->opencycmodel->getSuggestedPages($value);
-			foreach ($opencyc as $r){
-				$opencyc_uris[] = $r['uri'];
-			}
-		}	
-		if (isset($search_terms['opencycref']) == true) {
-			$opencyc_uris = array('http://sw.opencyc.org/concept/'.$search_terms['opencycref']);
+			$category = $search_terms['category'];
 		}
-		
-		if (isset($search_terms['category']) == true || isset($search_terms['opencycref']) == true) {		
-			foreach ($opencyc_uris as $uri) {
-				$results = array_merge($results, $this->lcamodel->getLCAsByCategory($uri));
-			}
-			$count = 0;
-			foreach ($results as $result) {
-				if ($count > $limit+$offset) {
-					break;
-				}
-				$tst = $limit+$offset;
-				if ($count >= $offset && $count < $limit+$offset) {
-				$impacts = array (
-					'uri' => $result['uri'],
-					'impactAssessments' => $this->lcamodel->convertImpactAssessments($this->lcamodel->getImpactAssessments($result['uri'])),
-					'quantitativeReference' => $this->lcamodel->convertQR($this->lcamodel->getQR($result['uri']))
-					);
-				$rs[$result['uri']] = $this->normalize($impacts);	
-				}
-				$count++;
-			}
-		} else {
-			$results = $this->lcamodel->getAllUsedCategories();
-			$rs = array();
-			foreach ($results as $result) {
-				if (strpos($result['uri'],"opencyc.org") !== false) {
-					$rs[] = array(
-						'opencycref' => str_replace("http://sw.opencyc.org/concept/","",$result['uri']),
-						'category' => $result['label']
-					);
-				}
-			}
+		// Querying the database for the footprints		
+		if($category){
+			$this->db->like('category', $category); 
 		}
+		if($name){
+			$this->db->like('name', $name); 
+		}		
+		$this->db->order_by("name", "ASC"); 
+		$rs = $this->db->get('footprints', $limit, $offset);
 		if ($encode == 'json') {
 			header('Content-type: application/json');
-			echo json_encode($rs);
+			echo json_encode($rs->result());
 		} else if ($encode == 'rdf') {
 			header('Content-type: text/xml');
-			echo $rs;			
+			echo $rs->result();			
 		} else if ($encode == 'xml') {
 			header('Content-type: text/xml');
-			echo $this->assocArrayToXML("results",$rs);
+			echo $this->assocArrayToXML("results",$rs->result());
 		} else if ($encode == 'html') {
 			header('Content-type: text/html');
-			echo $rs;			
+			var_dump($rs->result());			
 		}
+		
 		
 	}
-	
-	private function assocArrayToXML($root_element_name,$ar)
-	{
-		$this->load->library('Simplexml');
-	    $xml = new SimpleXMLElement("<?xml version=\"1.0\"?><{$root_element_name}></{$root_element_name}>");
-	    $f = create_function('$f,$c,$a','
-	            foreach($a as $k=>$v) {
-	                if(is_array($v)) {
-	                    $ch=$c->addChild($k);
-	                    $f($f,$ch,$v);
-	                } else {
-	                    $c->addChild($k,$v);
-	                }
-	            }');
-	    $f($f,$xml,$ar);
-	    return $xml->asXML();
-	}
-	
-	private function normalize($parts)
-	{
-		/* If the functional unit is mass, normalize to 1kg */
-		
-		if (strpos("Kilogram", $parts['quantitativeReference']['unit']['label']) !== false) {
-			$ratio = $parts['quantitativeReference']['amount'];
-			$parts['quantitativeReference']['amount'] = 1;
-			foreach ($parts['impactAssessments'] as &$impactAssessment) {
-				$impactAssessment['amount'] = $impactAssessment['amount'] / $ratio;
-			}
-		}
-		
-		if (strpos("Gram", $parts['quantitativeReference']['unit']['label']) !== false) {
-			$ratio = $parts['quantitativeReference']['amount'] / 1000;
-			// Remember to complete afterwards
-			$parts['quantitativeReference']['unit']['label'] = "Kilogram";
-			$parts['quantitativeReference']['amount'] = 1;
-			foreach ($parts['impactAssessments'] as &$impactAssessment) {
-				$impactAssessment['amount'] = $impactAssessment['amount'] / $ratio;
-				if (strpos("Gram", $impactAssessment['unit']['label']) !== false) { 
-					$impactAssessment['amount']/=1000; 
-					$impactAssessment['unit']['label'] = "Kilogram"; 
-					}
-			}
-		}
-		
-		// If ounces
-		if ($parts['quantitativeReference']['unit']['label'] == "http://data.nasa.gov/qudt/owl/unit#Ounce" ) {
-			$ratio = $parts['quantitativeReference']['amount'] / 0.028345;
-			// Remember to complete afterwards
-			$parts['quantitativeReference']['unit']['label'] = "Kilogram";
-			$parts['quantitativeReference']['amount'] = 1;
-			foreach ($parts['impactAssessments'] as &$impactAssessment) {
-				$impactAssessment['amount'] = $impactAssessment['amount'] / $ratio;
-				if ($impactAssessment['unit']['label'] == "Gram") { $impactAssessment['amount']/=1000; $impactAssessment['unit']['label'] = "Kilogram"; }
-			}
-		}
-		
-		if ($parts['quantitativeReference']['unit']['label'] == "http://data.nasa.gov/qudt/owl/unit#Pound" ) {
-			$ratio = $parts['quantitativeReference']['amount'] / 0.45359237;
-			// Remember to complete afterwards
-			$parts['quantitativeReference']['unit']['label'] = "Kilogram";
-			$parts['quantitativeReference']['amount'] = 1;
-			foreach ($parts['impactAssessments'] as &$impactAssessment) {
-				$impactAssessment['amount'] = $impactAssessment['amount'] / $ratio;
-				if ($impactAssessment['unit']['label'] == "Gram") { $impactAssessment['amount']/=1000; $impactAssessment['unit']['label'] = "Kilogram"; }
-			}
-		}
-		return $parts;
-	}
-		
+
 }
--- application/controllers/search.php
+++ application/controllers/search.php
@@ -12,14 +12,50 @@
 class Search extends FT_Controller {
 	public function __construct() {
 		parent::__construct();
-		$this->load->model(Array('lcamodel', 'geographymodel', 'bibliographymodel','peoplemodel','commentsmodel','ecomodel','opencycmodel'));	
-		$this->load->library(Array('form_extended', 'xml'));
-		$this->load->helper(Array('nameformat_helper','linkeddata_helper'));
+		//$this->load->model(Array('lcamodel', 'geographymodel', 'bibliographymodel','peoplemodel','commentsmodel','ecomodel','opencycmodel'));	
+		//$this->load->library(Array('form_extended', 'xml'));
+		//$this->load->helper(Array('nameformat_helper','linkeddata_helper'));
 		$obj =& get_instance();    
         $obj->load->library(array('xml'));
         $this->ci =& $obj;
 	}
+
 	
+	public function index(){
+		parse_str($_SERVER['QUERY_STRING'],$_GET);
+		$search_terms = $_GET;
+		if (isset($search_terms['category']) == true) {
+			$this->db->like('category', $search_terms['category']);
+			$this->data("category", $search_terms['category']);
+			$this->db->order_by("name", "ASC"); 
+			$rs = $this->db->get('footprints',100,0);
+			$this->data("set", $rs->result());
+		}
+		if (isset($_POST["keyword"]) == true){
+			$keyword = $_POST["keyword"];
+		} else {
+			$keyword = "";
+		}
+		if ($keyword != "") {
+			$this->db->like('name', $keyword); 
+			$this->db->order_by("name", "ASC"); 
+			$rs = $this->db->get('footprints',100,0);
+			$this->data("set", $rs->result());
+		} 
+		$categories = array(
+			array("uri" => "chemical", "label" => "chemical compound"),
+			array("uri"=>"transport", "label"=> "transportation"),
+			array("uri"=>"textile","label"=> "textile"),
+			array("uri"=>"building","label"=> "building material"),
+			array("uri"=>"food",	"label"=> "food"),
+			array("uri"=>"commodity","label"=> "commodity"));
+
+		$this->data("search_term", $keyword);
+		$this->data("menu", $categories);
+		$this->display("Search","search_view");
+	}
+	
+/*	
 	public function keyword() {
 		$keyword = $_POST["keyword"];
 		if ($keyword != "") {
@@ -133,7 +169,40 @@ class Search extends FT_Controller {
 			$categories = array(
 				array(
 					"uri" => "Mx4rvUCoPtoTQdaZVdw2OtjsAg",
-					"label" => "chemical compound"
+					"label" => "chemical compound"		parse_str($_SERVER['QUERY_STRING'],$_GET);
+		$checked_URIs = array();
+		$search_terms = $_GET;
+		$limit = 20;
+		$offset = 0;
+		$value = null;
+		$URIs = array();
+		$rs = array();
+		$category = null;
+		$name = null;
+		if (isset($search_terms['limit']) == true) {
+			$limit = $search_terms['limit'];
+		}
+		if (isset($search_terms['offset']) == true) {
+			$offset = $search_terms['offset'];
+		}
+		if (isset($search_terms['name']) == true) {
+			$name = $search_terms['name'];
+		}	
+		if (isset($search_terms['encode']) == true) {
+			$encode = $search_terms['encode'];
+		}
+		if (isset($search_terms['category']) == true) {
+			$category = $search_terms['category'];
+		}
+		// Querying the database for the footprints		
+		if($category){
+			$this->db->like('category', $category); 
+		}
+		if($name){
+			$this->db->like('name', $name); 
+		}		
+		$this->db->order_by("name", "ASC"); 
+		$rs = $this->db->get('footprints', $limit, $offset);
 				),
 				array(
 					"uri"=>"Mx4rv-6HepwpEbGdrcN5Y29ycA",
@@ -230,8 +299,8 @@ class Search extends FT_Controller {
 		//$rs = $this->opencycmodel->getOpenCycSearchCategories("http://sw.opencyc.org/concept/".$uri);
 		foreach ($rs as $r) {
 			$menu .= '<a href="/search/category/'.str_replace("http://sw.opencyc.org/concept/","",$r['uri']).'">'.$r['label'].'</a><br />';
-		} */	
+		} 	
 		
-	}
+	} */
 	
 }
